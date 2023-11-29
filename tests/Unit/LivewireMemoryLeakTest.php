1a084fdf0a12f218f0a9f2cbde2b3164825513d6
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2023-11-29 22:58:20 +0000

    test: update with description

--- tests/Unit/LivewireMemoryLeakTest.php
+++ tests/Unit/LivewireMemoryLeakTest.php
@@ -7,4 +7,4 @@ uses(TestCase::class);
 it('can show the bug', function (){
     $this->get('/')
         ->assertStatus(200);
-})->repeat(2000);
+})->repeat(5000); // You might need to increase this number depending on how much memory is allocated to PHP.
